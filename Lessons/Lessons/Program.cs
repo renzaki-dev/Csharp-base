da34bf59bc73c60a3e5f7c6f60117a0d5d07e1e2
kkabulov <kbkabulov0225@gmail.com>
2025-08-07 01:23:39 +0500

    Convert str to int

--- Lessons/Lessons/Program.cs
+++ Lessons/Lessons/Program.cs
@@ -26,24 +26,100 @@ namespace Lessons
             //Console.WriteLine(variable4);
             //Console.WriteLine(variable5);
 
+            //-----------------------------------------------------------------
+
+
             //string data;
             //data = Console.ReadLine(); // Input
             //Console.WriteLine("Hello " + data);
 
-            Console.WriteLine("Первое число = ");
-            string data1 = Console.ReadLine();
-            int a = Convert.ToInt32(data);
+            //Console.WriteLine("Первое число = ");
+            //string data1 = Console.ReadLine();
+            //int a = Convert.ToInt32(data);
 
-            Console.WriteLine("Второе число = ");
-            string data2 = Console.ReadLine();
-            int b = Convert.ToInt32(data2);
-
-
-            int sum = a + b;
+            //Console.WriteLine("Второе число = ");
+            //string data2 = Console.ReadLine();
+            //int b = Convert.ToInt32(data2);
 
 
-            Console.WriteLine("Summary = " + sum);
+            //int sum = a + b;
+
+
+            //Console.WriteLine("Summary = " + sum);
+            //Console.ReadLine();
+
+            //-----------------------------------------------------------------
+
+
+            //string data;
+            //Console.WriteLine("number A = ");
+            //data = Console.ReadLine();
+            //int a = Convert.ToInt32(data);
+
+            //Console.WriteLine("number B = ");
+            //data = Console.ReadLine();
+            //int b = Convert.ToInt32(data);
+
+            //int sum = (a + b) / 2;
+
+            //Console.WriteLine(sum);
+            //Console.ReadLine();
+
+            //-----------------------------------------------------------------
+
+            //string data;
+            //Console.WriteLine("number A = ");
+            //data = Console.ReadLine();
+            //int a = Convert.ToInt32(data);
+
+            //Console.WriteLine("number B = ");
+            //data = Console.ReadLine();
+            //int b = Convert.ToInt32(data);
+
+            //int sum = (a + b) / 2;
+
+            //Console.WriteLine(sum);
+            //Console.ReadLine();
+
+            //-----------------------------------------------------------------
+
+            //string data;
+            //int a, b, c;
+
+            //Console.WriteLine("A = ");
+            //data = Console.ReadLine();
+            //a = Convert.ToInt32(data);
+
+            //Console.WriteLine("B = ");
+            //data = Console.ReadLine();
+            //b = Convert.ToInt32(data);
+
+            //Console.WriteLine("C = ");
+            //data = Console.ReadLine();
+            //c = Convert.ToInt32(data);
+
+            //int sum = a + b + c;
+            //int multiply = a * b * c;
+
+            //Console.WriteLine("Summary = " + sum);
+            //Console.WriteLine("Произведение = " + multiply);
+            //Console.ReadLine();
+
+            //-----------------------------------------------------------------
+
+            string data;
+            int dollor;
+            double som = 12.625;
+
+            Console.WriteLine("Введите сумму в Доллaрах: ");
+            data = Console.ReadLine();
+            dollor = Convert.ToInt32(data);
+
+            double result = dollor * som;
+            Console.WriteLine("Доллaр в сум: " + result);
+
             Console.ReadLine();
+
         }
     }
 }
